522527718524d971853082f0bce8188cbb353624
allarviinamae <allar.viinamae@hotmail.com>
2017-05-02 17:49:31 +0300

    chore(): Updated Ionic

---
+++ src/pages/group-detail-chart/group-detail-chart.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'group-detail-chart',
+    template: ``
+})
+export class GroupDetailChart {
+
+    constructor() {
+    }
+
+
+}
